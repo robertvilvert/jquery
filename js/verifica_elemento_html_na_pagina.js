80b6d249473ae1330dc3d83d9caa57e8b93f740e
Robert Vilvert <robert.lvilvert@gmail.com>
2018-01-11 21:07:14 -0200

    Seletores. Selecionando e verificando se existe classe

---
+++ js/verifica_elemento_html_na_pagina.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    if($('.botao').length >0){
+      // Sempre retorna um array, mesmo que seja vazio, por isso nao é possivel comparar usando == (equal)
+      alert ("Tem class botao na pagina");
+    }
+});
